c424240ba13b1eb4d4fe3466f43fb54658a7de11
August <august.enhager@gmail.com>
2021-11-09 23:47:32 +0100

    WIP BMI logic

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,9 +1,13 @@
 describe("User can check their Bmi", () => {
   it("is expected that the user can enter weight and height", () => {
-    cy.visit("http://localhost:3001");
+    cy.visit("http://localhost:3000");
     cy.contains("BMI Calculator");
     cy.get("#weight").type("82");
     cy.get("#height").type = "183";
     cy.get("#Calculate").click();
+  });
+
+  it('is expected to display a BMI value of 26.01', () => {
+    cy.get("#results").should("contain", "26.01")
   });
 });
