a6e967de2f96b09f16cf465b79299429c0355be6
August <august.enhager@gmail.com>
2021-11-10 00:27:27 +0100

    Adds message corresponding BMI health

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -7,7 +7,11 @@ describe("User can check their Bmi", () => {
     cy.get("#Calculate").click();
   });
 
-  it('is expected to display a BMI value of 24.47', () => {
-    cy.get("#results").should("contain", "24.47")
+  it('is expected to display a BMI value of 24.49', () => {
+    cy.get("#results").should("contain", "24.49")
+  });
+
+  it('is expected to display a message if you are Healthy', () => {
+    cy.get("#results").should("contain", "Healthy")
   });
 });
