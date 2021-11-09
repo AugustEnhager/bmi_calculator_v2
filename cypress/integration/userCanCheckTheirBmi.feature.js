c14fec0edd00376cf638989da652222c0ba5a3fc
August <august.enhager@gmail.com>
2021-11-09 23:30:54 +0100

    Adds weight and height input placeholder with tests

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -2,5 +2,7 @@ describe("User can check their Bmi", () => {
   it("is expected that the user can enter weight and height", () => {
     cy.visit("http://localhost:3001");
     cy.contains('BMI Calculator')
+    cy.get("#weight").type("82")
+    cy.get("#height").type=("183")
   });
 });
