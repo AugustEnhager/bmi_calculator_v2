d61166e7ce76df90cb7d8bcd6a443744b0a9192e
August <august.enhager@gmail.com>
2021-11-09 23:34:43 +0100

    Adds Calculate button with test

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,8 +1,9 @@
 describe("User can check their Bmi", () => {
   it("is expected that the user can enter weight and height", () => {
     cy.visit("http://localhost:3001");
-    cy.contains('BMI Calculator')
-    cy.get("#weight").type("82")
-    cy.get("#height").type=("183")
+    cy.contains("BMI Calculator");
+    cy.get("#weight").type("82");
+    cy.get("#height").type = "183";
+    cy.get("#Calculate").click();
   });
 });
